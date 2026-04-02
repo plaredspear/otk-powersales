a27d740c851c2de1be41b4b570f38c58ea4fa232
soolimy <plaredspear@gmail.com>
2026-04-02 10:50:22 +0900

    refactor(backend): ProfessionalPromotionTeamHistory 엔티티 레거시 어노테이션 적용 (500)

--- backend/src/main/kotlin/com/otoki/internal/promotion/entity/ProfessionalPromotionTeamHistory.kt
+++ backend/src/main/kotlin/com/otoki/internal/promotion/entity/ProfessionalPromotionTeamHistory.kt
@@ -1,10 +1,13 @@
 package com.otoki.internal.promotion.entity
 
+import com.otoki.internal.common.salesforce.SFField
+import com.otoki.internal.common.salesforce.SFObject
 import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
 @Table(name = "professional_promotion_team_history")
+@SFObject("ProfessionalPromotionTeamHistory__c")
 class ProfessionalPromotionTeamHistory(
 
     @Id
@@ -12,15 +15,19 @@ class ProfessionalPromotionTeamHistory(
     @Column(name = "professional_promotion_team_history_id")
     val id: Long = 0,
 
+    @SFField("EmployeeId__c")
     @Column(name = "employee_id", nullable = false)
     val employeeId: Long,
 
+    @SFField("oldValue__c")
     @Column(name = "old_value", length = 50)
     val oldValue: String? = null,
 
+    @SFField("newValue__c")
     @Column(name = "new_value", nullable = false, length = 50)
     val newValue: String,
 
+    @SFField("updateTime__c")
     @Column(name = "changed_at", nullable = false)
     val changedAt: LocalDateTime = LocalDateTime.now()
 )
